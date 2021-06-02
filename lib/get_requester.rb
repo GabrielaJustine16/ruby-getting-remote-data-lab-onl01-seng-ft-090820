c7e8ccc5d37cf4a2aecc6c913cea05d729d49b3e
Gabriela johnson <johnsongabriela12@gmail.com>
2021-06-02 20:12:44 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -21,4 +21,5 @@ class GetRequester
     JSON.parse(get_response_body)
     
   end
+  get_requester = GetRequester.new('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
 end 
