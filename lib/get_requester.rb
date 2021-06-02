2fd064b2654eaf67866f466acaf88ac4b7d014be
Gabriela johnson <johnsongabriela12@gmail.com>
2021-06-02 20:04:08 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -18,24 +18,13 @@ class GetRequester
   end
   
   def parse_json
+    JSON.parse(get_response_body)
     
   end
   
   
   
   
-  
-  
-  
-  def get_response_body
-    uri = URI.parse(URL)
-    response = Net::HTTP.get_response(uri)
-    response.body
-  end
-  
-  def parse_json
-    JSON.parse(get_response_body)
-  end
   
   
   
